55075a5141e5a594e0dcac3f69dfafb487b15e0c
Virginia Garcia <ve.garcia.ortiz@gmail.com>
2019-05-16 19:25:03 -0400

    Workout pages

--- templates/single-plan-tabs.php
+++ templates/single-plan-tabs.php
@@ -8,10 +8,10 @@
 
 <div id="single-plan-nav">
 	<ul class="nav nav-pills">
-		<li class="active"><a class="overview-tab" href="#">Overview</a></li>
-		<li><a class="warp-up-tab" href="#">Warm-up</a></li>
-		<li><a class="workout-tab" href="#">Workout</a></li>
-		<li><a class="meal-plan-tab" href="#">Meal Plan</a></li>
-		<li><a class="recipes-tab" href="#">Recipes</a></li>
+		<li class="active"><a class="overview-tab" href="#"><?php esc_html_e( 'Overview', 'lsx-health-plan' ); ?></a></li>
+		<li><a class="warp-up-tab" href="#"><?php esc_html_e( 'Warm-up', 'lsx-health-plan' ); ?></a></li>
+		<li><a class="workout-tab" href="#"><?php esc_html_e( 'Workout', 'lsx-health-plan' ); ?></a></li>
+		<li><a class="meal-plan-tab" href="#"><?php esc_html_e( 'Meal Plan', 'lsx-health-plan' ); ?></a></li>
+		<li><a class="recipes-tab" href="#"><?php esc_html_e( 'Recipes', 'lsx-health-plan' ); ?></a></li>
 	</ul>
 </div>
