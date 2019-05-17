37e2b11e37f613242bf1a7eaaf8d3f0b4f7e127b
Virginia Garcia <ve.garcia.ortiz@gmail.com>
2019-05-17 12:38:12 -0400

    Recipes page

--- templates/tab-content-recipes.php
+++ templates/tab-content-recipes.php
@@ -26,7 +26,103 @@
 				'link_after'  => '</span>',
 			) );
 		?>
+		<div class="single-plan-section-title recipes-plan">
+			<h2 class="title-lined">Recipes <span class="blue-title">Day 1</span></h2>
+		</div>
+		<div class="single-plan-inner recipes-content">
+			<div class="recipes">
+				<div class="row eating-row">
+					<div class="col-md-4 recipe-column">
+						<div class="content-box box-shadow">
+							<h3 class="recipe-title title-lined">Marinated Hake</h3>
+							<table class="recipe-table">
+								<tbody>
+									<tr>
+										<td>Prep time:</td>
+										<td>30 min</td>
+									</tr>
+									<tr>
+										<td>Cooking time:</td>
+										<td>30 min</td>
+									</tr>
+									<tr>
+										<td>Serves:</td>
+										<td>30 min</td>
+									</tr>
+									<tr>
+										<td>Portion size:</td>
+										<td>30 min</td>
+									</tr>
+								</tbody>
+							</table>
+							<a href="#" class="btn btn-full">View Recipe</a>
+						</div>
+					</div>
+					<div class="col-md-4 recipe-column">
+						<div class="content-box box-shadow">
+							<h3 class="recipe-title title-lined">Fish Creole</h3>
+							<table class="recipe-table">
+								<tbody>
+									<tr>
+										<td>Prep time:</td>
+										<td>30 min</td>
+									</tr>
+									<tr>
+										<td>Cooking time:</td>
+										<td>30 min</td>
+									</tr>
+									<tr>
+										<td>Serves:</td>
+										<td>30 min</td>
+									</tr>
+									<tr>
+										<td>Portion size:</td>
+										<td>30 min</td>
+									</tr>
+								</tbody>
+							</table>
+							<a href="#" class="btn btn-full">View Recipe</a>
+						</div>
+					</div>
+					<div class="col-md-4 recipe-column">
+						<div class="content-box box-shadow">
+							<h3 class="recipe-title title-lined">Creamed Tuna</h3>
+							<table class="recipe-table">
+								<tbody>
+									<tr>
+										<td>Prep time:</td>
+										<td>30 min</td>
+									</tr>
+									<tr>
+										<td>Cooking time:</td>
+										<td>30 min</td>
+									</tr>
+									<tr>
+										<td>Serves:</td>
+										<td>30 min</td>
+									</tr>
+									<tr>
+										<td>Portion size:</td>
+										<td>30 min</td>
+									</tr>
+								</tbody>
+							</table>
+							<a href="#" class="btn btn-full">View Recipe</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div><!-- .entry-content -->
+
+	<div class="single-plan-inner-buttons">
+		<div class="complete-plan-btn">
+			<a class="btn cta-btn" href="#"><?php esc_html_e( 'COMPLETE DAY', 'lsx-health-plan' ); ?></a>
+		</div>
+		<div  class="back-plan-btn">
+			<a class="btn" href="#"><?php esc_html_e( 'BACK TO MY PLAN', 'lsx-health-plan' ); ?></a>
+		</div>
+	</div>
 
 	<footer class="footer-meta clearfix">
 		<?php if ( has_tag() || class_exists( 'LSX_Sharing' ) || ( function_exists( 'sharing_display' ) || class_exists( 'Jetpack_Likes' ) ) ) : ?>
