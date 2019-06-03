38775d884f60d1b4247a2a3641cefc8b8abb796b
Warwick Booth <warwick@lsdev.biz>
2019-06-03 18:04:03 +0200

    Made the workout page dynamic

--- templates/single-plan-tabs.php
+++ templates/single-plan-tabs.php
@@ -8,9 +8,27 @@
 <div id="single-plan-nav">
 	<ul class="nav nav-pills">
 		<li class="<?php lsx_health_plan_nav_class( '' ); ?>"><a class="overview-tab" href="<?php the_permalink(); ?>"><?php esc_html_e( 'Overview', 'lsx-health-plan' ); ?></a></li>
-		<li class="<?php lsx_health_plan_nav_class( 'warm-up' ); ?>"><a class="warm-up-tab" href="<?php the_permalink(); ?>warm-up/"><?php esc_html_e( 'Warm-up', 'lsx-health-plan' ); ?></a></li>
-		<li class="<?php lsx_health_plan_nav_class( 'workout' ); ?>"><a class="workout-tab" href="<?php the_permalink(); ?>workout/"><?php esc_html_e( 'Workout', 'lsx-health-plan' ); ?></a></li>
-		<li class="<?php lsx_health_plan_nav_class( 'meal' ); ?>"><a class="meal-plan-tab" href="<?php the_permalink(); ?>meal/"><?php esc_html_e( 'Meal Plan', 'lsx-health-plan' ); ?></a></li>
-		<li class="<?php lsx_health_plan_nav_class( 'recipes' ); ?>"><a class="recipes-tab" href="<?php the_permalink(); ?>recipes/"><?php esc_html_e( 'Recipes', 'lsx-health-plan' ); ?></a></li>
+		<?php
+		if ( lsx_health_plan_has_warmup() ) {
+			?>
+				<li class="<?php lsx_health_plan_nav_class( 'warm-up' ); ?>"><a class="warm-up-tab" href="<?php the_permalink(); ?>warm-up/"><?php esc_html_e( 'Warm-up', 'lsx-health-plan' ); ?></a></li>				
+			<?php
+		}
+		if ( lsx_health_plan_has_workout() ) {
+			?>
+				<li class="<?php lsx_health_plan_nav_class( 'workout' ); ?>"><a class="workout-tab" href="<?php the_permalink(); ?>workout/"><?php esc_html_e( 'Workout', 'lsx-health-plan' ); ?></a></li>
+			<?php
+		}
+		if ( lsx_health_plan_has_meal() ) {
+			?>
+				<li class="<?php lsx_health_plan_nav_class( 'meal' ); ?>"><a class="meal-plan-tab" href="<?php the_permalink(); ?>meal/"><?php esc_html_e( 'Meal Plan', 'lsx-health-plan' ); ?></a></li>
+			<?php
+		}
+		if ( lsx_health_plan_has_recipe() ) {
+			?>
+				<li class="<?php lsx_health_plan_nav_class( 'recipes' ); ?>"><a class="recipes-tab" href="<?php the_permalink(); ?>recipes/"><?php esc_html_e( 'Recipes', 'lsx-health-plan' ); ?></a></li>
+			<?php
+		}
+		?>
 	</ul>
 </div>
--- templates/tab-content-workout.php
+++ templates/tab-content-workout.php
@@ -4,15 +4,6 @@
  *
  * @package lsx-health-plan
  */
-
-$args     = array(
-	'orderby'        => 'date',
-	'order'          => 'DESC',
-	'post_type'      => 'workout',
-	'posts_per_page' => 1,
-);
-$workouts = new WP_Query( $args );
-
 ?>
 
 <?php lsx_entry_before(); ?>
@@ -37,125 +28,151 @@ $workouts = new WP_Query( $args );
 		?>
 		<div class="single-plan-inner workout-content">
 			<div class="single-plan-section-title workout">
-				<h2 class="title-lined">My Workout <span class="blue-title">Day 1</span></h2>
+				<h2 class="title-lined"><?php esc_html_e( 'My Workout', 'lsx-health-plan' ); ?> <span class="blue-title"><?php the_title(); ?></span></h2>
 			</div>
-			<div class="workout-instructions">
-				<div class="row">
-					<div class="col-md-6">
-					<h3>Dont forget your warm up!</h3>
-					<p>Be sure to do the warm-up before every workout session.</p>
-					</div>
-					<div class="col-md-6">
-						<div class="single-plan-inner-buttons">
-							<div class="complete-plan-btn">
-								<a class="btn border-btn" href="#"><?php esc_html_e( 'Download Warm-Up', 'lsx-health-plan' ); ?></a>
-							</div>
-							<div  class="back-plan-btn">
-								<a class="btn secondary-btn" href="<?php the_permalink(); ?>warm-up/"><?php esc_html_e( 'See Warm-Up', 'lsx-health-plan' ); ?></a>
+			<?php
+			if ( lsx_health_plan_has_warmup() ) {
+				?>
+				<div class="workout-instructions">
+					<div class="row">
+						<div class="col-md-6">
+							<h3>Dont forget your warm up!</h3>
+							<p>Be sure to do the warm-up before every workout session.</p>
+						</div>
+						<div class="col-md-6">
+							<div class="single-plan-inner-buttons">
+								<div class="complete-plan-btn">
+									<a class="btn border-btn" href="#"><?php esc_html_e( 'Download Warm-Up', 'lsx-health-plan' ); ?></a>
+								</div>
+								<div  class="back-plan-btn">
+									<a class="btn secondary-btn" href="<?php the_permalink(); ?>warm-up/"><?php esc_html_e( 'See Warm-Up', 'lsx-health-plan' ); ?></a>
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-			</div>
+				<?php
+			}
+			?>
 			<div class="set content-box">
 				<?php
-				$i = 1;
-				if ( $workouts->have_posts() ) :
-					while ( $workouts->have_posts() ) :
+				$connected_workouts = get_post_meta( get_the_ID(), 'connected_workouts', true );
+				if ( empty( $connected_workouts ) ) {
+					return;
+				}
+				$args = array(
+					'orderby'   => 'date',
+					'order'     => 'DESC',
+					'post_type' => 'workout',
+					'post__in'  => $connected_workouts,
+				);
+				$workouts = new WP_Query( $args );
+
+				if ( $workouts->have_posts() ) {
+					while ( $workouts->have_posts() ) {
 						$workouts->the_post();
-						$post_id         = get_the_id();
-						$workout_section = 'workout_section_' . ( $i ) . '_title';
-						$workout_desc    = 'workout_section_' . ( $i ) . '_description';
-						$section_title   = get_post_meta( get_the_ID(), $workout_section, true );
-						$description     = get_post_meta( get_the_ID(), $workout_desc, true );
 
-						$group_name = 'workout_section_' . $i;
-						$group      = get_post_meta( get_the_ID(), $group_name, true );
+						$i = 1;
+						$section_counter = 6;
+						while ( $i <= $section_counter ) {
 
-						if ( isset( $group[0]['name'] ) ) {
-							$workout_name = esc_html( $group[0]['name'] );
-						}
-						if ( isset( $group[0]['reps'] ) ) {
-							$workout_reps = esc_html( $group[0]['reps'] );
-						}
-						if ( isset( $group[0]['connected_videos'] ) ) {
-							$workout_video = esc_html( $group[0]['video'] );
-						}
-						$giphy    = get_post_meta( get_the_ID(), 'video_giphy_source', true );
-						$youtube  = esc_url( get_post_meta( get_the_ID(), 'video_youtube_source', 1 ) );
-						?>
-						<h3 class="set-title"><?php echo esc_html( $section_title ); ?></h3>
-						<div class="set-content">
-							<p><?php echo esc_html( $description ); ?></p>
-						</div>
-						<div class="set-table">
-							<table class="workout-table">
-								<tbody>
-									<tr>
-										<th>Workout</th> 
-										<th class="center-mobile">Reps / Time / Distance</th>
-										<th class="center-mobile">Video</th>
-									</tr>
-									<tr>
-										<td class="workout-title-item"><?php echo esc_html( $workout_name ); ?></td>
-										<td class="reps-field-item center-mobile"><?php echo esc_html( $workout_reps ); ?></td>
-										<td class="video-button-item center-mobile">
-											<a href="#" data-toggle="modal" data-target="#exampleModal">
-												<span class="fa fa-play-circle"></span>
-											</a>
-										</td>
-									</tr>
-									<tr>
-										<td class="workout-title-item">Back lunge to hammer curl press</td>
-										<td class="reps-field-item center-mobile">12REPS</td>
-										<td class="video-button-item center-mobile">
-											<a href="#" data-toggle="modal" data-target="#exampleModal">
-												<span class="fa fa-play-circle"></span>
-											</a>
-										</td>
-									</tr>
-									<tr>
-										<td class="workout-title-item">Speed skaters</td>
-										<td class="reps-field-item center-mobile">45 REPS</td>
-										<td class="video-button-item center-mobile">
-											<a href="#" data-toggle="modal" data-target="#exampleModal">
-												<span class="fa fa-play-circle"></span>
-											</a>
-										</td>
-									</tr>
-								</tbody>
-							</table>
-						</div>
+							$workout_section = 'workout_section_' . ( $i ) . '_title';
+							$workout_desc    = 'workout_section_' . ( $i ) . '_description';
+							$section_title   = get_post_meta( get_the_ID(), $workout_section, true );
+							$description     = get_post_meta( get_the_ID(), $workout_desc, true );
 
-						<!-- Modal -->
-						<div class="modal" id="exampleModal" tabindex="-1" role="dialog">
-							<div class="modal-dialog" role="document">
-								<div class="modal-content">
-									<div class="modal-header">
-										<h5 class="modal-title"><?php echo esc_html( $workout_name ); ?></h5>
-										<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-										<span aria-hidden="true">&times;</span>
-										</button>
-									</div>
-									<div class="modal-body">
-									<?php
-									if ( ! empty( $giphy ) ) {
-										echo $giphy; // WPCS: XSS OK.
-									} elseif ( ! empty( $youtube ) ) {
-										echo wp_oembed_get( $youtube, array( // WPCS: XSS OK.
-											'width' => 480,
-										) );
-									}
-									?>
+							if ( '' === $section_title ) {
+								$i++;
+								continue;
+							}
+							?>
+
+							<h3 class="set-title"><?php echo esc_html( $section_title ); ?></h3>
+							<div class="set-content">
+								<p><?php echo esc_html( $description ); ?></p>
+							</div>
+
+							<?php
+							$group_name = 'workout_section_' . $i;
+							$groups     = get_post_meta( get_the_ID(), $group_name, true );
+
+							if ( ! empty( $groups ) ) {
+								?>
+								<div class="set-table">
+									<table class="workout-table">
+										<tbody>
+											<tr>
+												<th>Workout</th> 
+												<th class="center-mobile">Reps / Time / Distance</th>
+												<th class="center-mobile">Video</th>
+											</tr>
+											<?php
+											foreach( $groups as $group ) {
+												$workout_name = '';
+												if ( isset( $group['name'] ) ) {
+													$workout_name = esc_html( $group['name'] );
+												}
+												$workout_reps = '';
+												if ( isset( $group['reps'] ) ) {
+													$workout_reps = esc_html( $group['reps'] );
+												}
+												$workout_video = '';
+												$giphy         = '';
+												$youtube       = '';
+												if ( isset( $group['connected_videos'] ) ) {
+													$workout_video = esc_html( $group['video'] );
+													$giphy    = get_post_meta( $workout_video, 'video_giphy_source', true );
+													$youtube  = esc_url( get_post_meta( $workout_video, 'video_youtube_source', 1 ) );
+												}
+												?>
+												<tr>
+													<td class="workout-title-item"><?php echo esc_html( $workout_name ); ?></td>
+													<td class="reps-field-item center-mobile"><?php echo esc_html( $workout_reps ); ?></td>
+													<td class="video-button-item center-mobile">
+														<a href="#" data-toggle="modal" data-target="#exampleModal">
+															<span class="fa fa-play-circle"></span>
+														</a>
+													</td>
+												</tr>
+												<?php
+											}
+											?>
+										</tbody>
+									</table>
+								<?php
+							}
+							?>
+							</div>
+							<!-- Modal -->
+							<div class="modal" id="exampleModal" tabindex="-1" role="dialog">
+								<div class="modal-dialog" role="document">
+									<div class="modal-content">
+										<div class="modal-header">
+											<h5 class="modal-title"><?php echo esc_html( $workout_name ); ?></h5>
+											<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+											<span aria-hidden="true">&times;</span>
+											</button>
+										</div>
+										<div class="modal-body">
+										<?php
+										if ( ! empty( $giphy ) ) {
+											echo $giphy; // WPCS: XSS OK.
+										} elseif ( ! empty( $youtube ) ) {
+											echo wp_oembed_get( $youtube, array( // WPCS: XSS OK.
+												'width' => 480,
+											) );
+										}
+										?>
+										</div>
 									</div>
 								</div>
 							</div>
-						</div>
-						<!-- End Modal -->
-
-						<?php $i++; ?>
-					<?php endwhile; ?>
-				<?php endif; ?>
+							<!-- End Modal -->
+							<?php
+							$i++;
+						}
+					} ?>
+				<?php } ?>
 				<?php wp_reset_postdata(); ?>
 			</div>
 		</div>
