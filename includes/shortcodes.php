b0a537a049e5a538d96ff2aef62bea23799add34
Warwick Booth <warwick@lsdev.biz>
2019-06-07 17:57:18 +0200

    Adding the slider enqueue

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -96,6 +96,8 @@ function feature_tips_box() {
 	ob_start();
 	echo lsx_health_plan_featured_tips_block(); // WPCS: XSS OK.
 	$content = ob_get_clean();
+	wp_enqueue_script( 'slick', LSX_HEALTH_PLAN_URL . 'assets/js/slick.min.js', array( 'jquery' ), LSX_HEALTH_PLAN_VER, true );
+	wp_enqueue_script( 'lsx-health-plan-slider', LSX_HEALTH_PLAN_URL . 'assets/js/lsx-health-plan-slider.min.js', array( 'slick' ), LSX_HEALTH_PLAN_VER, true );	
 	return $content;
 }
 
