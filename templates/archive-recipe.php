a1dff2a71223970cf6cd0f180cacfefa53f407e7
Virginia Garcia <ve.garcia.ortiz@gmail.com>
2020-01-17 19:22:03 -0300

    grids

--- templates/archive-recipe.php
+++ templates/archive-recipe.php
@@ -33,7 +33,7 @@ get_header(); ?>
 							while ( have_posts() ) :
 								the_post();
 								?>
-								<div class="col-md-4">
+								<div class="col-xs-12 col-sm-6 col-md-4">
 									<article class="lsx-slot box-shadow">
 										<div class="recipe-feature-img">
 											<?php
