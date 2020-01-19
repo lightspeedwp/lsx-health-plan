371b8713d2fc9fc2057c1c8c4be1c659eabcc658
Warwick Booth <warwick@lsdev.biz>
2020-01-19 02:54:48 +0200

    Added in the call for the recipe type description. #49

--- includes/template-tags.php
+++ includes/template-tags.php
@@ -630,12 +630,14 @@ function lsx_health_plan_workout_video_play_button( $m, $group ) {
 function lsx_health_plan_recipe_archive_description() {
 	if ( is_post_type_archive( 'recipe' ) ) {
 		$description = \lsx_health_plan\functions\get_option( 'recipe_archive_description', '' );
-		if ( '' !== $description ) {
-			?>
-			<div class="lsx-hp-archive-description row">
-				<div class="col-xs-12 description-wrapper"><?php echo wp_kses_post( $description ); ?></div>
-			</div>
-			<?php
-		}
+	} elseif ( is_tax() ) {
+		$description = get_the_archive_description();
+	}
+	if ( '' !== $description ) {
+		?>
+		<div class="lsx-hp-archive-description row">
+			<div class="col-xs-12 description-wrapper"><?php echo wp_kses_post( $description ); ?></div>
+		</div>
+		<?php
 	}
 }
