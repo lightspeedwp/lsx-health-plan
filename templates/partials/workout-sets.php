852eaa2fd8b80cd84f3554cdbc07eb5de2c89693
Warwick Booth <warwick@lsdev.biz>
2020-05-12 19:34:41 +0200

    Adding in a lsx_health_plan_workout_sets template which calls the workout-sets.php template.

---
+++ templates/partials/workout-sets.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Template used to loop through the workout sets and display the workouts.
+ *
+ * @package lsx-health-plan
+ */
+global $group_name;
+?>
+<div class="sets">
+	<?php
+	$connected_workouts = get_post_meta( get_the_ID(), 'connected_workouts', true );
+	if ( empty( $connected_workouts ) ) {
+		$options = \lsx_health_plan\functions\get_option( 'all' );
+		if ( isset( $options['connected_workouts'] ) && '' !== $options['connected_workouts'] && ! empty( $options['connected_workouts'] ) ) {
+			$connected_workouts = $options['connected_workouts'];
+			if ( ! array( $connected_workouts ) ) {
+				$connected_workouts = array( $connected_workouts );
+			}
+		}
+	}
+	$args     = array(
+		'orderby'   => 'date',
+		'order'     => 'DESC',
+		'post_type' => 'workout',
+		'post__in'  => $connected_workouts,
+	);
+	$workouts = new WP_Query( $args );
+
+	if ( $workouts->have_posts() ) {
+		while ( $workouts->have_posts() ) {
+			$workouts->the_post();
+			$i               = 1;
+			$section_counter = 6;
+			while ( $i <= $section_counter ) {
+
+				$workout_section = 'workout_section_' . ( $i ) . '_title';
+				$workout_desc    = 'workout_section_' . ( $i ) . '_description';
+
+				$section_title = get_post_meta( get_the_ID(), $workout_section, true );
+				$description   = get_post_meta( get_the_ID(), $workout_desc, true );
+
+				if ( '' === $section_title ) {
+					$i++;
+					continue;
+				}
+				?>
+				<div class="set-box set content-box">
+					<h3 class="set-title"><?php echo esc_html( $section_title ); ?></h3>
+					<div class="set-content">
+						<p><?php echo wp_kses_post( apply_filters( 'the_content', $description ) ); ?></p>
+					</div>
+					<?php lsx_health_plan_workout_tab_content( $i ); ?>
+				</div>
+				<?php
+				$i++;
+			}
+		}
+		?>
+	<?php } ?>
+	<?php wp_reset_postdata(); ?>
+</div>
+<?php
