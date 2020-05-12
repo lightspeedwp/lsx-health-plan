94ff272b962e38cfb85920297e45cf724e81b1e9
Warwick Booth <warwick@lsdev.biz>
2020-05-12 20:38:33 +0200

    Allowing the content for the warm up tab.

--- templates/tab-content-warm-up.php
+++ templates/tab-content-warm-up.php
@@ -33,6 +33,23 @@ if ( false !== $warm_up && '' !== $warm_up ) {
 			lsx_entry_before();
 			if ( 'workout' === get_post_type() ) {
 				$connected_workouts = array( get_the_ID() );
+				?>
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<?php lsx_entry_top(); ?>
+					<div class="entry-content">
+						<?php
+							the_content();
+							wp_link_pages( array(
+								'before'      => '<div class="lsx-postnav-wrapper"><div class="lsx-postnav">',
+								'after'       => '</div></div>',
+								'link_before' => '<span>',
+								'link_after'  => '</span>',
+							) );
+						?>
+					</div><!-- .entry-content -->
+					<?php lsx_entry_bottom(); ?>
+				</article><!-- #post-## -->
+				<?php
 				lsx_health_plan_workout_sets();
 			} else {
 				?>
