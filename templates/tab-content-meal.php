ab7e5e0ab7afa83c1a4413e28c3cc50bb5727b39
Virginia Garcia <ve.garcia.ortiz@gmail.com>
2020-07-30 17:09:20 -0400

    Editing the Meal tab

--- templates/tab-content-meal.php
+++ templates/tab-content-meal.php
@@ -4,6 +4,9 @@
  *
  * @package lsx-health-plan
  */
+
+global $shortcode_args;
+
 ?>
 
 <?php lsx_entry_before(); ?>
@@ -15,15 +18,26 @@
 	</div><!-- .entry-meta -->
 
 	<div class="entry-content">
-		<div class="single-plan-section-title meal-plan">
-			<?php lsx_get_svg_icon( 'meal.svg' ); ?>
-			<h2 class="title-lined"><?php esc_html_e( 'My Meal Plan', 'lsx-health-plan' ); ?> <span class="blue-title"><?php the_title(); ?></span></h2>
-		</div>
 		<div class="single-plan-inner meal-content">
+			<div class="single-plan-section-title meal-plan title-lined">
+				<?php lsx_get_svg_icon( 'meal.svg' ); ?>
+				<h2><?php esc_html_e( 'My Meal Plan', 'lsx-health-plan' ); ?> <?php the_title(); ?></h2>
+			</div>
 			<?php require LSX_HEALTH_PLAN_PATH . 'templates/partials/meal-plans.php'; ?>
+
 		</div>
 	</div><!-- .entry-content -->
-
+	<?php if ( null === $shortcode_args ) { ?>
+		<div class="row tip-row extras-box">
+			<?php if ( post_type_exists( 'tip' ) && lsx_health_plan_has_tips() ) { ?>
+				<div class="col-md-4">
+					<div class="tip-right">
+						<?php echo do_shortcode( '[lsx_health_plan_featured_tips_block]' ); ?>
+					</div>
+				</div>
+			<?php } ?>
+		</div>
+	<?php } ?>
 	<footer class="footer-meta clearfix">
 		<?php if ( has_tag() || class_exists( 'LSX_Sharing' ) || ( function_exists( 'sharing_display' ) || class_exists( 'Jetpack_Likes' ) ) ) : ?>
 			<div class="post-tags-wrapper">
