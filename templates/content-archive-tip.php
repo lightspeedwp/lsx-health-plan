016bd8b88c89f202aebfc46485714451bdcbd6ad
Virginia Garcia <ve.garcia.ortiz@gmail.com>
2020-08-10 11:36:33 -0400

    changing the tips icon

--- templates/content-archive-tip.php
+++ templates/content-archive-tip.php
@@ -12,7 +12,7 @@ $featured_image = get_the_post_thumbnail( $tip_id, array( 600, 300 ) );
 <div class="content-box box-shadow diet-tip-wrapper quick-tip">
 	<div class="row">
 		<div class="col-lg-1 col-md-1 col-sm-1 col-xs-2">
-			<?php lsx_get_svg_icon( 'meal.svg' ); ?>
+			<?php lsx_get_svg_icon( 'tips-icon.svg' ); ?>
 		</div>
 		<div class="col-lg-11 col-md-11 col-sm-11 col-xs-10">
 			<h3 class="tip-title"><?php the_title(); ?></h3>
