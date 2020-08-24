ef51b9ca64b1bf52efa57845f320892d073d2d8d
Warwick Booth <warwick@lsdev.biz>
2020-08-24 15:43:45 +0200

    Moved `hp_add_body_classes` and renamed to `body_classes`

--- classes/frontend/class-general.php
+++ classes/frontend/class-general.php
@@ -21,7 +21,7 @@ class General {
 	 * Contructor
 	 */
 	public function __construct() {
-
+		add_action( 'body_class', array( $this, 'body_classes' ) );
 	}
 
 	/**
@@ -37,5 +37,44 @@ class General {
 			self::$instance = new self();
 		}
 		return self::$instance;
+	}
+
+	/**
+	 * Add body classes to body.
+	 *
+	 * @param array $classes
+	 * @return void
+	 */
+	public function body_classes( $classes = array() ) {
+		global $post;
+
+		if ( isset( $post->post_content ) && has_shortcode( $post->post_content, 'lsx_health_plan_my_profile_block' ) ) {
+			$classes[] = 'my-plan-shortcode';
+		}
+
+		if ( is_single() && is_singular( 'plan' ) ) {
+			$args = array(
+				'post_parent' => get_the_ID(),
+				'post_type'   => 'plan',
+			);
+
+			$post_id      = get_the_ID();
+			$has_children = get_children( $args );
+			$has_parent   = wp_get_post_parent_id( $post_id );
+
+			if ( ! empty( $has_children ) ) {
+				$plan_type_class = 'parent-plan-page';
+				if ( 0 !== $has_parent ) {
+					$plan_type_class = 'parent-sub-plan-page';
+				}
+			} else {
+				$plan_type_class = 'unique-plan-page';
+				if ( 0 !== $has_parent ) {
+					$plan_type_class = 'child-plan-page';
+				}
+			}
+			$classes[] = $plan_type_class;
+		}
+		return $classes;
 	}
 }
