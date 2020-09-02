5b8f3188daf8d2a00637b46a86a6d6b3d75b7102
Virginia Garcia <ve.garcia.ortiz@gmail.com>
2020-09-02 11:42:13 -0400

    workout style fixes

--- classes/integrations/class-articles.php
+++ classes/integrations/class-articles.php
@@ -73,12 +73,12 @@ class Articles {
 					'desc'       => __( 'Connect the related articles that applies to this ', 'lsx-health-plan' ) . $default_type,
 					'id'         => $default_type . '_connected_articles',
 					'type'       => 'post_search_ajax',
-					'limit'      => 3,  // Limit selection to X items only (default 1).
-					'sortable'   => true, // Allow selected items to be sortable (default false).
+					'limit'      => 3,
+					'sortable'   => true,
 					'query_args' => array(
 						'post_type'      => array( 'post' ),
 						'post_status'    => array( 'publish' ),
-						'posts_per_page' => -1,
+						'posts_per_page' => 3,
 					),
 				)
 			);
